9f00679118ef3900adffcb703c5bc84747a96943
Chris Simon <info@decomputeur.nl>
2024-03-13 16:49:39 +0100

    Update combine.sh

--- combine.sh
+++ combine.sh
@@ -4,10 +4,15 @@
 for file in *.html; do
     # Skip header and footer files
     if [[ "$file" != "head.html" && "$file" != "foot.html" ]]; then
+        # Output the filename being processed
+        echo "Combining header and footer with $file..."
+
         # Combine header and footer with HTML file
         cat head.html > tmp.html
         cat "$file" >> tmp.html
         cat foot.html >> tmp.html
         mv tmp.html "$file"
+
+        echo "Successfully combined header and footer with $file."
     fi
 done
